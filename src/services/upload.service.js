a9e8d33d47396003053ec45a5c9a04f22027cc70
Trian Saputra <trian4869@gmail.com>
2026-08-24 11:36:28 +0700

    feat: menambahkan file filtering di upload

--- src/services/upload.service.js
+++ src/services/upload.service.js
@@ -2,11 +2,11 @@ const multer = require("multer");
 const path = require("path");
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
+  destination: (_req, _file, cb) => {
     cb(null, "uploads/");
   },
 
-  filename: (req, file, cb) => {
+  filename: (_req, file, cb) => {
     const uniqueName =
       `${Date.now()}-${Math.round(Math.random() * 1e9)}` +
       path.extname(file.originalname);
@@ -15,8 +15,28 @@ const storage = multer.diskStorage({
   },
 });
 
+const fileFilter = (_req, file, cb) => {
+  const allowedExtensions = [".jpg", ".jpeg", ".png", ".webp"];
+
+  const extension = path.extname(file.originalname).toLowerCase();
+
+  console.log("File:", file.originalname);
+  console.log("Mimetype:", file.mimetype);
+  console.log("Extension:", extension);
+
+  if (!allowedExtensions.includes(extension)) {
+    return cb(new Error("File harus berupa JPG, PNG, atau WEBP"));
+  }
+
+  cb(null, true);
+};
+
 const upload = multer({
   storage,
+  fileFilter,
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
 });
 
 module.exports = {
